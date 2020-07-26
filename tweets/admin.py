e6016aa93d2a108a33e9c6a38ec206f1be7c1ee3
Evgeny Ocean <evgeny.ocean@gmail.com>
2020-07-26 13:22:59 +0300

    admin tweaking and db tweet-user relationship added

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tweet
 
 # Register your models here.
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'user') #any field names of the model to display (you can't use filtering, cuz it's looking only for field names, specifically for its __str__)
+    search_fields = ['content', 'user__username'] #looking inside the specified fields values (you can and probably must use filtering in foreign keys)
+
+admin.site.register(Tweet, TweetAdmin)
--- tweets/models.py
+++ tweets/models.py
@@ -1,10 +1,12 @@
 import random
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Tweet(models.Model):
     content = models.TextField(null=True, blank=True)
     image = models.FileField(upload_to='images/', null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ["-id"]
