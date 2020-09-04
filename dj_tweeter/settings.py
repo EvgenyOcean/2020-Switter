ba57da568689c74499ae403bface84b9ff816223
Evgeny Ocean <evgeny.ocean@gmail.com>
2020-09-04 10:38:41 +0300

    aws s3

--- dj_tweeter/settings.py
+++ dj_tweeter/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     #third parties 
     'rest_framework',
     'corsheaders',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -175,3 +176,17 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3001",
     "http://127.0.0.1:9000",
 ]
+
+#AWS BUCKETS 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+AWS_S3_REGION_NAME = 'eu-west-2'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_ADDRESSING_STYLE = "virtual"
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
